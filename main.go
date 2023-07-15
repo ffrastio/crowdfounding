b8455f70c43a8d322e7ee79d791d5d27d80f101c
ffrastio <48426886+ffrastio@users.noreply.github.com>
2023-07-15 08:26:45 +0700

    go first

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/bwa_crowdfounding?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Koneksi berhasil")
+}
