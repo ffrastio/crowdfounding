43b92e38b01a1b0822bd4cfdc976758db7461558
ffrastio <48426886+ffrastio@users.noreply.github.com>
2023-07-16 15:19:00 +0700

    JSON user

---
+++ entity/user.go
@@ -0,0 +1,17 @@
+package entity
+
+import "time"
+
+type User struct {
+	ID           int32
+	Name         string
+	Occupation   string
+	Role         string
+	Avatar       string
+	PasswordHash string
+	Email        string
+	Visible      string
+	Token        string
+	CreatedAt    time.Time
+	UpdateddAt   time.Time
+}
--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"bwastartup/entity"
 	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	router := gin.Default()
+
+	router.GET("/users", UserHandler)
+	router.Run()
+}
+
+func UserHandler(ctx *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/bwa_crowdfounding?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err.Error())
 	}
 	fmt.Println("Koneksi berhasil")
+
+	var users []entity.User
+
+	db.Find(&users)
+
+	ctx.JSON(http.StatusOK, users)
 }
